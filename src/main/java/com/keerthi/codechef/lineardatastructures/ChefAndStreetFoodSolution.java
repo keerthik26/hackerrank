1aec47728ab2e59cecc9d11aec0b4e5e298762b9
keerthik26 <k.keerthi71@yahoo.com>
2020-09-01 12:21:18 +0530

    CodeChef problems

--- src/main/java/com/keerthi/codechef/lineardatastructures/ChefAndStreetFoodSolution.java
+++ src/main/java/com/keerthi/codechef/lineardatastructures/ChefAndStreetFoodSolution.java
@@ -1,25 +1,35 @@
 package com.keerthi.codechef.lineardatastructures;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ChefAndStreetFoodSolution {
 
     public static void main(String[] args) {
+        runTests();
         Scanner in = new Scanner(System.in);
         int numOfTests = Integer.parseInt(in.nextLine());
         for (int i = 0; i < numOfTests; i++) {
             int numOfFoodItems = Integer.parseInt(in.nextLine());
-           // ArrayList<StreetFood> streetFoods = new ArrayList<>();
-            int maxProfit = 0;
+            ArrayList<StreetFood> streetFoods = new ArrayList<>();
             for (int j = 0; j < numOfFoodItems; j++) {
                 String[] foodDetails = in.nextLine().split(" ");
-                //streetFoods.add(new StreetFood(Integer.parseInt(foodDetails[0]),Integer.parseInt(foodDetails[1]),Integer.parseInt(foodDetails[2])));
-                maxProfit = Math.max(new StreetFood(Integer.parseInt(foodDetails[0]),Integer.parseInt(foodDetails[1]),Integer.parseInt(foodDetails[2])).getDailyProfitForNewStore(),maxProfit);
+                streetFoods.add(new StreetFood(Integer.parseInt(foodDetails[0]),Integer.parseInt(foodDetails[1]),Integer.parseInt(foodDetails[2])));
             }
-            System.out.println(maxProfit);
+            System.out.println(getMaxProfit(streetFoods));
         }
     }
+
+   public static int getMaxProfit(List<StreetFood> foodList) {
+      return foodList.stream().mapToInt(StreetFood::getDailyProfitForNewStore).max().getAsInt();
+   }
+
+   public static void runTests(){
+      assert getMaxProfit(Arrays.asList(new StreetFood(4,6,8),new StreetFood(1,4,3),new StreetFood(2,6,6))) == 12 : "Should be 12";
+      assert getMaxProfit(Arrays.asList(new StreetFood(7,7,4))) == 0 : "Should be 0";
+   }
 }
 class StreetFood{
     int numOfStores;
